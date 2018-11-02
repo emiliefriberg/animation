ffc1e49b48fa37c10a1a97ee6ca331b12c679871
Emilie Friberg <emiliefriberg@desktop-544o9it.kea.local>
2018-11-02 11:59:35 +0100

    Tilføjelse af position og point

--- script.js
+++ script.js
@@ -7,15 +7,29 @@ function sidenVises() {
 }
 
 
+
+
+
+
+
 function showStart() {
     console.log("showstart");
+
+
+
 
     //vis startskærm
     document.querySelector("#start").classList.add("show");
 
 
+
+
     //start animation på start-knap
     document.querySelector("#start_knap").classList.add("pulse");
+
+
+
+
 
 
     //henter hideStart når der bliver klikket på knappen
@@ -24,14 +38,27 @@ function showStart() {
 }
 
 
+
+
+
+
 function hideStart() {
     console.log("hideStart");
+
+
+
 
     //stop animation på start-knap
     document.querySelector("#start_knap").classList.add("hide");
 
+
+
+
     //fade startskærm ud
     document.querySelector("#start").classList.add("fade_out");
+
+
+
 
     //henter startGame når animationen fade_out er færdig
     document.querySelector("#start").addEventListener("animationend", startGame);
@@ -44,19 +71,28 @@ function hideStart() {
 function startGame() {
     console.log("startGame");
 
-    //skjul startskærm
 
+
+
+
+    //skjul startskærm
     document.querySelector("#start").classList.add("hide");
     document.querySelector("#start").classList.remove("show");
 
 
-    //vis spilskærm
 
+
+
+    //vis spilskærm
     document.querySelector("#game_background").classList.add("show");
 
 
-    //start falling
 
+
+
+
+
+    //start falling
     document.querySelector("#cigaretpakke").classList.add("falling");
 
     document.querySelector("#daase_graa").classList.add("falling");
@@ -78,22 +114,46 @@ function startGame() {
     document.querySelector("#pindsvin").classList.add("falling");
 
 
+
+
+
+
+
+
     //klik på skrald
 
-    document.querySelector("#plastik").addEventListener("click", clickSkrald);
+    document.querySelector("#cigaretpakke").addEventListener("click", clickSkrald)
+    document.querySelector("#daase_graa").addEventListener("click", clickSkrald)
     document.querySelector("#daase_orange").addEventListener("click", clickSkrald);
 
-    //klik på dyr
+    document.querySelector("#plastik").addEventListener("click", clickSkrald);
+    document.querySelector("#plastikflaske").addEventListener("click", clickSkrald);
+    document.querySelector("#plastikpose").addEventListener("click", clickSkrald);
 
+    document.querySelector("#solvpapir").addEventListener("click", clickSkrald);
+
+
+
+
+
+
+
+
+
+    //klik på dyr
     document.querySelector("#kanin").addEventListener("click", clickDyr);
     document.querySelector("#pindsvin").addEventListener("click", clickDyr);
+
+    document.querySelector("#ugle").addEventListener("click", clickDyr);
 
 }
 
 
+
+
+
+
 //Når der klikkes på skrald
-
-
 
 let point = 0;
 
@@ -106,14 +166,28 @@ function clickSkrald() {
     point++;
     console.log(point);
 
-    document.querySelector(".antal_point").textContent = point
+    document.querySelector(".antal_point").textContent = point;
+
+
+    console.log(this);
+
+    let myNumber = Math.floor((Math.random() * 5) + 1);
+    console.log("tilfældigt tal: " + myNumber);
+
+
+    this.classList = "skrald skrald_pos" + myNumber;
 
 }
 
 
+
+
+
+
+
 //Når der klikkes på dyr
 
-let liv = 3;
+let life = 3;
 
 
 function clickDyr() {
@@ -121,41 +195,96 @@ function clickDyr() {
 
     // TODO: mist et liv
 
+    life--;
+    console.log(life);
+    document.querySelector(".antal_liv").textContent = life;
 
-    document.querySelector(".antal_liv").textContent = liv
-    point--;
-    console.log(liv);
+    console.log(this);
+
+    let myNumber = Math.floor((Math.random() * 5) + 1);
+    console.log("tilfældigt tal: " + myNumber);
+
+
+    this.classList = "dyr dyr_pos" + myNumber;
 
 }
 
+
+
+
+
+
+
+//Skrald forsvinder
 
 function skraldGone() {
+
     console.log("skraldGone");
 
-    document.querySelector("#plastik").classList.add("paused");
+    //    document.querySelector("#plastik").classList.add("paused");
+    document.querySelector("#cigaretpakke").classList.add("dissappear");
+
+    document.querySelector("#daase_graa").classList.add("dissappear");
+
+    document.querySelector("#daase_orange").classList.add("dissappear");
+
     document.querySelector("#plastik").classList.add("dissappear");
+
+    document.querySelector("#plastikflaske").classList.add("dissappear");
+
+    document.querySelector("#plastikpose").classList.add("dissappear");
+
+    document.querySelector("#solvpapir").classList.add("dissappear");
+
+
+
+
+
+
+
+
+    //Der klikkes på skraldet
+    document.querySelector("#cigaretpakke").addEventListener("click", skraldGone);
+
+    document.querySelector("#daase_graa").addEventListener("click", skraldGone);
+
+    document.querySelector("#daase_orange").addEventListener("click", skraldGone);
+
+    document.querySelector("#plastik").addEventListener("click", skraldGone);
+
+    document.querySelector("#plastikflaske").addEventListener("click", skraldGone);
+
+    document.querySelector("#plastikpose").addEventListener("click", skraldGone);
+
+    document.querySelector("#solvpapir").addEventListener("click", skraldGone);
 
 }
 
-document.querySelector("#plastik").addEventListener("click", skraldGone);
 
 
 
 
 
-
-
-
+//Dyr forsvinder
 
 function dyrGone() {
     console.log("dyrGone");
 
-    document.querySelector("#kanin").classList.add("paused");
+    //    document.querySelector("#kanin").classList.add("paused");
     document.querySelector("#kanin").classList.add("dissappear");
 
-}
+    document.querySelector("#pindsvin").classList.add("dissappear");
 
-document.querySelector("#kanin").addEventListener("click", dyrGone);
+    document.querySelector("#ugle").classList.add("dissappear");
+
+    document.querySelector("#kanin").addEventListener("click", dyrGone);
+
+    document.querySelector("#pindsvin").addEventListener("click", dyrGone);
+
+    document.querySelector("#ugle").addEventListener("click", dyrGone);
+
+
+}
 
 
 
